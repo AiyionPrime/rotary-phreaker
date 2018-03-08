9244be2892d1e860a8bab6fbe0eed62b59726086
aiyion.prime <git@aiyionpri.me>
2018-03-08 15:40:14 +0100

    moved tests

--- rotary-phreaker/setup.py
+++ rotary-phreaker/setup.py
@@ -4,4 +4,7 @@ from distutils.core import setup
 setup(name='rotary-phreaker',
       version='0.1',
       py_modules=['rotary-phreaker'],
+      install_requires=[
+          'RPi.GPIO'
+      ]
       )
